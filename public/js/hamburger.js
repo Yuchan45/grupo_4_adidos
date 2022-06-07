a1610a894baa1f904b8290edbdfab880652ea950
Yuchan <yu.nakasone@gmail.com>
2022-06-07 17:32:21 -0300

    Termine el resonsive del header

---
+++ public/js/hamburger.js
@@ -0,0 +1,36 @@
+// Hamburger Menu interactions
+const labels = document.querySelectorAll('.aside-menu nav a');
+let menuBtn = document.getElementById("ham-menu");
+let hamInner = document.querySelector('.ham-inner');
+let aside = document.querySelector('.aside-menu');
+let menuOpen = false;
+
+function disableOverflow(flag) {
+    // Recibe como parametro un booleano. Si es true, deshabilita el scroll. Si es false, lo habilita.
+    let body = document.querySelector('body');
+    if (flag == true){
+        body.style.overflowY = 'hidden';
+    } else {
+        body.style.overflowY = 'auto';
+    }
+}
+
+menuBtn.addEventListener('click', function() {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        aside.classList.add('active');
+        menuOpen = true;
+    }else {
+        menuBtn.classList.remove('open');
+        aside.classList.remove('active');
+        menuOpen = false;
+    }
+});
+
+labels.forEach(function (label) {
+    // Recide un array de labels y les agrega el evento onClick, el cual esconde el menuAside.
+    label.addEventListener('click', () => {
+        menuBtn.classList.remove('open');
+        aside.classList.remove('active');
+    });
+});
