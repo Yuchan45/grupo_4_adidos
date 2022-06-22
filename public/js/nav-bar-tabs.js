03e4b80f2dcb7662fd7cd6e9a45a35f105dbd906
Yuchan <yu.nakasone@gmail.com>
2022-06-22 00:40:43 -0300

    Arregle otro bud del header. Implemente el reseteo de los tabs

--- public/js/nav-bar-tabs.js
+++ public/js/nav-bar-tabs.js
@@ -1,37 +1,47 @@
 const dropdownButtons = document.querySelectorAll('#drop-btn');
 
-document.addEventListener('click', e => {
-        // console.log(e.target.className)
-        const isDropdownBtn = e.target.matches('[data-dropdown-button]');
-        const notifElement = e.target.parentElement.parentElement;
-        const isNotificationBtn = notifElement.classList.contains('notification');
-
-        if (!(isDropdownBtn || isNotificationBtn)) {
-            dropdownButtons.forEach(tab => {
-                let tabText = tab.childNodes[1];
-                let dropdownContainer = tab.nextElementSibling;
-                if (dropdownContainer.classList.contains('dropdown-active')) {
-                    tabText.classList.remove('active-tab');
-                    dropdownContainer.classList.remove('dropdown-active');
-                }
-            });
-        }
-});
-
+document.addEventListener('click', unToggleDropdown);
 
 dropdownButtons.forEach(button => {
     button.addEventListener('click', toggleDropdown);
 });
 
 function toggleDropdown(e) {
-    //console.log(this); // Siempre retorna el elemento drop-btn
     let tabText = this.childNodes[1];
     dropdownMenu = this.nextElementSibling;
-
+    tabReset();
     tabText.classList.add('active-tab');
     dropdownMenu.classList.add('dropdown-active');
 }
 
+function unToggleDropdown(e) {
+    // Si, ya se que esta hecho a lo negro pero no se por que haciendolo como se debe no me toma el notification... 
+    // No toma cuando le agrego el data-dropdown-button al notification...
+    const isDropdownBtn = e.target.matches('[data-dropdown-button]');
+    const notifElement = e.target.parentElement.parentElement;  // Xq el target al hacer click sobre el notif, agarra el svg... Asi que hay que buscar el abuelo
+    const isNotificationBtn = notifElement.classList.contains('notification');
+
+    if (!(isDropdownBtn || isNotificationBtn)) {
+        dropdownButtons.forEach(tab => {
+            let tabText = tab.childNodes[1];
+            let dropdownContainer = tab.nextElementSibling;
+            if (dropdownContainer.classList.contains('dropdown-active')) {
+                tabText.classList.remove('active-tab');
+                dropdownContainer.classList.remove('dropdown-active');
+            }
+        });
+    }
+}
+
+function tabReset() {
+    dropdownButtons.forEach(tab => {
+        // Debo resetear los tabs al clickear en alguno.
+        let tabText = tab.childNodes[1];
+        let dropdownContainer = tab.nextElementSibling;
+        tabText.classList.remove('active-tab');
+        dropdownContainer.classList.remove('dropdown-active');
+    });
+}
 
 
 // ****************** Addressing *******************
