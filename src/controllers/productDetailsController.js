28cd0cc0a5b0260f380b4e516c0562002a8ea83d
FrancoEstevez <73672444+FrancoEstevez@users.noreply.github.com>
2022-06-26 19:56:07 -0300

    clone y cree nueva branch

--- src/controllers/productDetailsController.js
+++ src/controllers/productDetailsController.js
@@ -1,7 +1,26 @@
+const sneakersData = require('../data/sneakers');
+
 const prodDetailsController = {
     landing: function(req, res) {
-        //res.sendFile('product-details.html', {root: './src/views/'});
-        res.render('product-details');
+        res.render('product-details', {sneakers: sneakersData});
+    },
+    obtenerPorId: (req, res) => {
+        const productId = parseInt(req.params.id, 10);
+        let productoEncontrado; 
+
+        for (let i = 0; i < sneakersData.length; i++) {
+            if ( sneakersData[i].id === productId ) {
+                productoEncontrado = sneakersData[i];
+            }
+        }
+        
+        if (!productoEncontrado){
+            res.status(404).send("No se encuentra el producto");
+        }
+        else {
+            res.render( "product-details",  {sneakers: productoEncontrado});
+        }
+
     }
 };
 
--- src/routes/productDetails.js
+++ src/routes/productDetails.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const prodDetailsController = require('../controllers/productDetailsController');
 
 router.get('/', prodDetailsController.landing);
+router.get('/:id', prodDetailsController.obtenerPorId);
+
 
 module.exports = router;
