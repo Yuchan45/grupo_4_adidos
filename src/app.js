8de570923fff64b65afa458c742ae11cb6d2508a
Yuchan <yu.nakasone@gmail.com>
2022-06-29 20:48:47 -0300

    Instale el paquete del method override

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
 const app = express();
+
+// METHOD OVERRRIDE
+// override with the X-HTTP-Method-Override header in the request
+app.use(methodOverride('X-HTTP-Method-Override'))
 
 // ROUTES
 const mainRoutes = require('./routes/mainRoutes');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
-
 
 // STATIC PATHS
 const publicPath = path.resolve(__dirname, '../public');
@@ -17,7 +21,6 @@ app.use(express.static(__dirname + './public'));
 
 // SET TEMPLATE ENGINE (EJS)
 app.set('view engine', 'ejs');
-
 
 // ADDRESSING
 app.use('/', mainRoutes);
