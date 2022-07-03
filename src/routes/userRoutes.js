64080da280bf77d50e474381974eaf30dbb74537
Yuchan <yu.nakasone@gmail.com>
2022-07-03 20:36:45 -0300

    Hice que se puedan subir imagenes al sv. Ya puedo recibir el resto de los datos desde el form. No se comko hacer para obtener el id de la imagen (el cual esta en el filename del storage en userRoutes). No se como ahcer para que llegue al controller dicha data...

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,35 @@
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+const multer = require('multer');
+
+
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/usersController');
+
+// Seteo donde y como guardar los archivos.
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        let folder = path.join(__dirname, '../../public/images/profiles');
+        cb(null, folder);
+    },
+    filename: function(req, file, cb) {
+        //let imageName = Date.now() + path.extname(file.originalname);
+        let imageName = uuidv4() + path.extname(file.originalname);
+        cb(null, imageName);
+        // avatar = imageName;
+    }
+});
+
+let fileUpload = multer({storage: storage});
+
+
+router.get('/login', usersController.login);
+
+router.get('/register', usersController.register);
+router.post('/register', fileUpload.single('profileImage'), usersController.createUser); // le paso profileImage que seria el name del input file.
+
+router.get('/recover', usersController.recover);
+router.get('/list', usersController.list);
+
+module.exports = router;
