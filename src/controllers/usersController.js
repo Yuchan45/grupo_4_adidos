af97079c94b39abe22e8dd9c7fe8c71ffb03a574
Yuchan <yu.nakasone@gmail.com>
2022-07-04 20:42:56 -0300

    Hice que se pueda crear un perfil con o sin imagen. Seteando una imagen de perfil default en caso de no tenerla

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,7 +1,6 @@
 const userCrud = require('./usersModules/fileControl');
 const { v4: uuidv4 } = require('uuid');
 
-
 const usersController = {
     login: function(req, res) {
         res.render('./users/login-form');
@@ -17,12 +16,17 @@ const usersController = {
         res.render('./users/user-list', {users: users});
     },
     createUser: function(req, res) {
+        let profileImage = '';
+        const file = req.file;
+
+        file ? profileImage = req.file.filename : "default.png";
+
         let user = {
             id: uuidv4(),
             accCreationDate: new Date().toISOString().slice(0, 10),
             name: req.body.name,
             username: req.body.username,
-            avatarImageName: req.file.filename,
+            avatarImageName: profileImage,
             bannerName: "",
             email: req.body.email,
             address: req.body.address,
@@ -33,7 +37,7 @@ const usersController = {
             interests: req.body.interest
         };
         userCrud.saveUser(user);
-        res.redirect('/');
+        res.redirect('/user/list');
     }
 };
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,20 +8,19 @@ const router = express.Router();
 const usersController = require('../controllers/usersController');
 
 // Seteo donde y como guardar los archivos.
-const fileStoratgeEngineConfig = multer.diskStorage({
+const fileStorageEngineConfig = multer.diskStorage({
     destination: function(req, file, cb) {
         let folder = path.join(__dirname, '../../public/images/profiles');
         cb(null, folder);
     },
     filename: function(req, file, cb) {
         //let imageName = Date.now() + path.extname(file.originalname);
-        let imageName = uuidv4() + path.extname(file.originalname);
+        let imageName = 'userProfile-' + uuidv4() + path.extname(file.originalname);
         cb(null, imageName);
-        // avatar = imageName;
     }
 });
 
-let fileUpload = multer({storage: fileStoratgeEngineConfig});
+let fileUpload = multer({storage: fileStorageEngineConfig});
 
 
 router.get('/login', usersController.login);
