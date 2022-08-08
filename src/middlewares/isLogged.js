c4cda3ab0f6df4bd06f0d0101369cf651c3e2517
Yuchan <yu.nakasone@gmail.com>
2022-08-08 19:53:45 -0300

    Arregle un bug del sessions. Ya no se puede modificar usuarios que no te pertenecen, y el problema de que se borraba el sessions era por culpa de nodemon que refreseheaba y borraba la sesion

--- src/middlewares/isLogged.js
+++ src/middlewares/isLogged.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 function isLogged(req, res, next) {
     if (req.session.activeUser == undefined) {
-        res.send("Debes estar logueado para entrar aqui");
+        res.send("Debes estar logueado para entrar aqui " + req.session.activeUser);
     } else {
         next();
         return;
