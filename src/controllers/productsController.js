8f443dd7cc26d49afb0c549a2fe16c4347e81067
FrancoEstevez <73672444+FrancoEstevez@users.noreply.github.com>
2022-09-11 19:10:05 -0300

    hice CRUD para products

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,165 +1,91 @@
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
+let db = require("../database/models");
 
-const fileOperation = require('../modules/fileControl');
-const userFunction = require('../modules/userFunction');
-const productFunction = require('../modules/productFunction');
-
-const sneakersData = require('../data/sneakers');
-const allShoesPath = path.join(__dirname, '../data/products.json');
-const allShoes = fileOperation.readFile(allShoesPath);
-
-const productsController = {
-    
-    allProducts: function (req, res) {
-        updateProducts = fileOperation.readFile(allShoesPath);
-        res.render('./products/all-products', { 
-            trendingSneakers: sneakersData,
-            products: updateProducts,
-            user: req.session.userLogged
-        });
-    },
-    editProdIndex: function (req, res) {
-        const prodId = req.params.id;
-        const allShoes = fileOperation.readFile(allShoesPath);
-        let editProduct = productFunction.getProdById(allShoes, prodId);
-
-        res.render('./products/edit-products', {
-            user: req.session.userLogged,
-            data : editProduct,
-            msg : ''
-        })
-    },
-    editProduct: function (req, res) {
-        const id = req.params.id;
-        const file = req.file;
-        const product = req.body;
-        const allShoes = fileOperation.readFile(allShoesPath);
-        let editProduct = productFunction.getProdById(allShoes, id);
-
-        if (!file) {
-            const msg = "Debe seleccionar una imagen de producto!";
-            res.render('./products/edit-products', {
-                user: req.session.userLogged,
-                data : editProduct,
-                msg : msg
-            })
-            return;
-        }
-        //console.log(file.filename)
-     
-        const updatedProduct = {
-            id: editProduct.id,
-            prodCreationDate: editProduct.prodCreationDate, //dia que cree producto
-            // productOwner:  
-            brand: product.brand,
-            model: product.model,
-            category: product.category,
-            description: product.description,
-            price: product.price,
-            discount: product.discount,
-            image: file.filename,
-            color: product.color,
-            gender: product.gender,
-            stock: product.stock
-        };
-
-        // Remove image files.
-        productFunction.removeProductImage(allShoes, id);
-
-        // Creo un nuevo array sin el elemento modificado.
-        let updatedArray = productFunction.removeProdFromArray(allShoes, id);
-        updatedArray.push(updatedProduct);
-        fileOperation.writeFile(updatedArray, allShoesPath);
-
-        res.redirect('/products');
-    },
-    obtenerPorId: (req, res) => {
-        const productId = req.params.id;
-        //console.log(productId);
-        let productoEncontrado;
-
-        for (let i = 0; i < allShoes.length; i++) {
-            //console.log(allShoes[i]);
-            if (allShoes[i].id === productId) {
-                productoEncontrado = allShoes[i];
-            }
-        }
-        //console.log(productoEncontrado)
-        if (!productoEncontrado) {
-            res.status(404).send("No se encuentra el producto");
-        } else {
-            res.render('./products/product-details',  {
-                sneakerEncontrado: productoEncontrado, 
-                trendingSneakers: sneakersData,
-                user: req.session.userLogged
-            });
-        }
-    },
+let productsController = {
     create: function (req, res) {
-        res.render('./products/createProduct', {
-            user: req.session.userLogged,
-            old: '',
-            errorMsg: ''
-        })
+        db.Category.findAll()
+            .then(function(category){
+                return res.render("createProduct", {category : category})
+            })
     },
-    // productList: function (req, res) {
-    //     res.render('./products/productsList', { products: allShoes });
-    // },
-    createProduct: function (req, res) {
-        const file = req.file;
-        const product = req.body;
 
-        const newProduct = {
-            id: uuidv4(),
-            prodCreationDate: new Date().toISOString().slice(0, 10), //dia que cree producto
-            productOwner: req.session.userLogged.name,
-            brand: product.brand,
-            model: product.model,
-            category: product.category,
-            description: product.description,
-            price: product.price,
-            discount: product.discount,
-            image: file.filename,
-            color: product.color,
-            gender: product.gender,
-            stock: product.stock
-        };
-        
-        fileOperation.addToFile(newProduct, allShoesPath);
-        res.redirect('/products');
+    save: function (req, res){
+        db.Product.create({
+            model: req.body.model,
+            brand: req.body.brand,
+            description: req.body.description,
+            price: req.body.price,
+            discount: req.body.discount,
+            gender: req.body.gender,
+            category: req.body.category,
+            color: req.body.color,
+            size: req.body.size,
+            stock: req.body.stock,
+            // No deja "product-image"
+            productimage: req.body.product-image
+        });
+
+        //esta bien poner "/create"?
+        res.redirect("/create")
     },
-    search: function(req, res) {
-        // const activeUser = fileOperation.readFile(activeUserFile);
-        const allShoes = fileOperation.readFile(allShoesPath);
 
-        let userSearch = req.query.search;
-        let productsResults = [];
+    productsList: function (req, res){
+        db.Product.findAll()
+            .then(function(products){
+                res.render("productsList", {products:products})
+            })
+    }, 
+    
+    // le cambie el nombre de product-details a productDetails
+    productDetails: function (req, res){
+        db.Product.findByPk(req.params.id)
+        /* Esperando a que se hagan las relaciones en models
+            include: [{association: "category"}, {association: "brand"}] */
+            .then(function(product){
+                res.render("productDetails", {product:product})
+            })
+    },
+    
+    editProducts: function (req, res) {
+        let productsRequest = db.Product.findByPk(req.params.id);
+        let categoryRequest = db.Category.findAll();
 
-        for (let i = 0; i < allShoes.length; i++) {
-            if (allShoes[i].brand.toLowerCase().includes(userSearch.toLowerCase())) {
-                productsResults.push(allShoes[i])
-            }
+        Promise.all([productsRequest, categoryRequest])
+            .then(function([product, category]){
+                res.render("editProducts", {product:product, category:category})
+            })
+    },
+
+    update: function (req, res) {
+        db.Product.update({
+            model: req.body.model,
+            brand: req.body.brand,
+            description: req.body.description,
+            price: req.body.price,
+            discount: req.body.discount,
+            gender: req.body.gender,
+            category: req.body.category,
+            color: req.body.color,
+            size: req.body.size,
+            stock: req.body.stock,
+            // No deja "product-image"
+            productimage: req.body.product-image
+    }, {
+        where: {
+            id: req.params.id
         }
-        res.render('./products/all-products', {
-            trendingSneakers : productsResults,
-            products : productsResults,
-            user: req.session.userLogged
+    });
+    
+    res.redirect("/products/" + req.params.id)
+},
+
+    delete: function(req, res){
+        db.Product.destroy({
+            where: {
+                id: req.params.id
+            }
         })
-    },
-    deleteProduct: function (req, res) {
-         const id = req.params.id;
-        if (!id) return;
-
-        let products = fileOperation.readFile(allShoesPath);
-        let newArray = productFunction.removeProdFromArray(products, id);
-        fileOperation.writeFile(newArray, allShoesPath);
-
-        // Remove image files.
-        productFunction.removeProductImage(products, id);
-
-        res.redirect('/products');
+        res.redirect("/products");
     }
-};
+}
 
 module.exports = productsController;
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -7,9 +7,23 @@ const productsController = require('../controllers/productsController');
 const uploadFile = require('../middlewares/products/productsMulter');
 const hasFile = require('../middlewares/products/hasFile');
 
+router.get('/prueba', productsController.prueba);
+
 router.get('/', productsController.allProducts);
 
 router.get('/create', productsController.create);
+
+// Create
+router.post('/create', productsController.save);
+// Read
+router.get('/', productsController.productsList);
+// Detalle 
+router.get('/:id', productsController.productDetails);
+// Update = actualizar
+router.get('/edit/:id', productsController.editProducts);
+router.post('/edit/:id', productsController.update);
+// Delete
+router.post("/delete/:id", productsController.delete);
 
 router.post('/create', uploadFile.single("product-image"), hasFile, productsController.createProduct); 
 
