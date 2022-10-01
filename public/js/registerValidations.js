136d84bdeed71649f6f374afc472e58f9d47ff1d
FrancoEstevez <73672444+FrancoEstevez@users.noreply.github.com>
2022-09-30 21:56:14 -0300

    register validations started

---
+++ public/js/registerValidations.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+    let label = document.querySelector("#prueba");
+
+    label.addEventListener("focus", function() {
+        console.log(1)
+    })
+})
