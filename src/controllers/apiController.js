218b13919ece563083ac0c054d038dfcde1440f1
FrancoEstevez <73672444+FrancoEstevez@users.noreply.github.com>
2022-10-06 23:58:19 -0300

    apis terminadas

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,6 +1,14 @@
+const db = require('../database/models')
 const DB = require('../database/models')
 const usersController = require('./usersController')
 const Op = DB.sequelize.Op
+
+const Products = db.Product;
+const Brands = db.Brand;
+const Categories = db.Category;
+const ShoppingCarts = db.Shopping_cart;
+const Items = db.Item;
+
 
 module.exports = {
     userList : (req, res) => {
@@ -26,19 +34,41 @@ module.exports = {
             })
     },
 
-    productList : (req, res) => {
-        DB.Product
-            .findAll()
-            .then(products => {
-                return res.json({
-                    count: products.length,
-                    countByCategory: {
-                        
-                    }, 
-                    data: products
-                })
-            })
+    productList : async (req, res) => {
+        let countByBrands = {};
+        let brandsArray = [];
+
+        const brands = await Brands.findAll();
+        brands.forEach(brand => {
+            brandsArray.push(brand.name);
+        });
+
+        const products = await Products.findAll({
+            include: [{association: "brands"}] 
+        });
+
+        for (let i=0; i<brandsArray.length; i++) {
+            let count = 0;
+            for (let j=0; j<products.length; j++) {
+                if (products[j].brands.name == brandsArray[i]) {
+                    let marca = products[j].brands.name;
+                    count += 1;
+                    countByBrands[marca] = count;
+                }
+            }
+        }
+
+        console.log(countByBrands);
+
+        const productos = await Products.findAll();
+        return res.json({
+            count: productos.length,
+            countByBrands,
+            data: productos
+        });
     },
+
+
     productListId : (req, res) => {
         DB.Product
             .findByPk(req.params.id)
